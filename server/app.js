e9fd4e057e6f883cb2bcb295760ef117a71e32ad
Tomas Carranza Casares <tomicarranza999@gmail.com>
2023-03-08 13:35:08 -0300

    bugfix express listen

--- server/app.js
+++ server/app.js
@@ -1,30 +1,35 @@
-import createError from 'http-errors'
-import express from 'express'
-import logger from 'morgan'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import createError from "http-errors";
+import express from "express";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import db from './sequelize/db/index.js'
+import routes from "./routes/index.js";
+import db from "./sequelize/db/index.js";
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(logger('dev'))
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use("/", routes);
 
-app.use((req, res, next) => next(createError(404)))
+app.use((req, res, next) => next(createError(404)));
 
 app.use((err, req, res, next) => {
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-  res.status(err.status || 500)
-  res.send(err)
-})
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.send(err);
+});
 
 db.sync({ force: true }).then(() => {
-  console.log('Db connected')
-  app.listen(app, PORT, () => {
-    console.log(`Server listening at port ${PORT}`)
-  })
-})
+  console.log("Db connected");
+  app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`);
+  });
+});
 
-export default app
+export default app;
