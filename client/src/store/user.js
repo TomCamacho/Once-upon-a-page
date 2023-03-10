1604089dc8d803a2deee1330236f67a40979407b
Ezequiel Birman <ebirman77@gmail.com>
2023-03-09 21:49:26 -0300

    bugfix: app starts with no logged in user

--- client/src/store/user.js
+++ client/src/store/user.js
@@ -1,18 +1,13 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
-export const logIn = createAction("LOG_IN");
-export const logOut = createAction("LOG_OUT");
+export const logIn = createAction('LOG_IN')
+export const logOut = createAction('LOG_OUT')
 
-const initialState = {
-  id: 1,
-  displayName: "Homero",
-  email: "homero@gmail.com",
-  password: "1234"
-};
+const initialState = null
 
 const userReducer = createReducer(initialState, {
   [logIn]: (state, action) => action.payload.data,
   [logOut]: (state, action) => (state = null),
-});
+})
 
-export default userReducer;
+export default userReducer
