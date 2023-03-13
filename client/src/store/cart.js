2a554385fe8b7e81b5fd0bfe8ae4319d9f0a2001
tomcamacho <tomas_ivan_camacho@hotmail.com>
2023-03-13 15:47:08 -0300

    fix redux cart

--- client/src/store/cart.js
+++ client/src/store/cart.js
@@ -12,27 +12,41 @@ const cartReducer = createReducer(initialState, {
   [addUnit]: (state, action) => {
     return state.map(product => {
       if (product.id === action.payload) {
-        return (product.units += 1)
+        return {
+          ...product,
+          units: product.units + 1,
+        }
       }
+      return product
     })
   },
   [subtractUnit]: (state, action) => {
     return state.map(product => {
       if (product.units === 1) {
-        return state.filter(product => product.id !== action.payload)
+        return {
+          ...product,
+          units: product.units,
+        }
       }
       if (product.id === action.payload) {
-        return (product.units -= 1)
+        return {
+          ...product,
+          units: product.units - 1,
+        }
       }
+      return product
     })
   },
   [newProduct]: (state, action) => {
-    state.map(product => {
-      if (product.id === action.payload.id) {
-        return (product.units += 1)
-      }
-    })
-    return [...state, action.payload]
+    const productExists = state.find(
+      product => product.id === action.payload.id
+    )
+    if (productExists) {
+      productExists.units += 1;
+      return [...state]
+    } else {
+      return [...state, action.payload]
+    }
   },
   [removeProduct]: (state, action) => {
     return state.filter(product => product.id !== action.payload)
