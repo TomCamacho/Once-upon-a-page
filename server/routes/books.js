223238f9a050464eee7a44ae328d9f8ee636e0a7
Rodrigo Castellanos <rodrigojoaquinc89@gmail.com>
2023-03-15 17:10:58 -0300

    add routes admin(user,book,genres) & model Genre

--- server/routes/books.js
+++ server/routes/books.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 import { Op } from 'sequelize'
 
-import { validateAuth } from '../middleware/auth.js'
+import { validateAuth, validateAdmin } from '../middleware/auth.js'
 import { Book } from '../sequelize/db/models/index.js'
 
 const router = Router()
@@ -14,34 +14,25 @@ const formatBookFromGoogle = book => ({
   googleId: book.id,
   title: book.volumeInfo.title,
   authors: book.volumeInfo.authors,
-  price: book.saleInfo.retailPrice.amount * 100,
   rating: book.volumeInfo.averageRating,
   images: [book.volumeInfo.imageLinks.thumbnail],
-  genres: [book.volumeInfo.categories],
   pages: book.volumeInfo.pageCount,
   publishingHouse: book.volumeInfo.publisher,
   published: book.volumeInfo.publishedDate,
   language: book.volumeInfo.language,
   description: book.volumeInfo.description,
-  stock: book.stock
+  stock: book.stock,
+  price: book.stock,
+  genres: book.genres,
 })
 
 router.get('/', async (req, res) => {
   let condition
   const { title, googleId } = req.query
-  if (title) condition = { title: {[Op.iLike]: `%${title}%`} }
+  if (title) condition = { title: { [Op.iLike]: `%${title}%` } }
   if (googleId) condition = { googleId }
   const books = await Book.findAll({ where: condition }, { include: 'Genre' })
   return res.status(200).send(books)
-})
-
-// TODO replace validateAuth with validateAdmin middleware
-router.post('/', validateAuth, async (req, res) => {
-  const { googleId, stock } = req.body
-  const response = await fetch(apiBaseURL.concat(googleId))
-  const bookFromGoogle = await response.json()
-  const bookToAdd = await Book.create(bookFromGoogle({...bookFromGoogle, stock}))
-  res.status(201).send(bookToAdd)
 })
 
 router.get('/search/:textToSearch', async (req, res) => {
@@ -57,6 +48,38 @@ router.get('/volume/:id', async (req, res) => {
   const response = await fetch(apiBaseURL.concat(id))
   const book = await response.json()
   res.status(200).send(formatBookFromGoogle(book))
+})
+
+// ----ADMIN----
+
+router.post('/', validateAuth, validateAdmin, async (req, res) => {
+  const { googleId, stock, price, genres } = req.body
+  const response = await fetch(apiBaseURL.concat(googleId))
+  const book = await response.json()
+  const bookToAdd = await Book.create(
+    bookForClient({ ...book, stock, price, genres })
+  )
+  res.status(201).send(bookToAdd)
+})
+
+router.put('/:id', validateAuth, validateAdmin, async (req, res) => {
+  const { id } = req.params
+  try {
+    await Book.update(req.body, { where: { id: id } })
+    res.sendStatus(202)
+  } catch (error) {
+    res.sendStatus(404)
+  }
+})
+
+router.delete('/:id', validateAuth, validateAdmin, async (req, res) => {
+  const { id } = req.params
+  try {
+    await Book.destroy({ where: { id: id } })
+    res.sendStatus(202)
+  } catch (error) {
+    res.sendStatus(404)
+  }
 })
 
 export default router
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,11 @@ import { Router } from 'express'
 
 import booksRouter from './books.js'
 import userRouter from './users.js'
+import genreRouter from './genres.js'
 
 const router = Router()
 router.use('/books', booksRouter)
 router.use('/user', userRouter)
+router.use('/genres', genreRouter)
 
 export default router
